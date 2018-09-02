292763c80521287982f130878e269a876fffe1b7
A5116083 <sunilsourabh@gmail.com>
2018-09-03 07:51:01 +0800

    some thing to commit

---
+++ com-ocbc-commonmodel/src/main/java/utils/ProductConverter.java
@@ -0,0 +1,15 @@
+package utils;
+
+import io.vertx.core.json.JsonObject;
+import models.Product;
+
+public class ProductConverter {
+
+    public static void fromJson(JsonObject json, Product obj){
+
+    }
+
+    public static void toJson(Product obj, JsonObject json){
+
+    }
+}
